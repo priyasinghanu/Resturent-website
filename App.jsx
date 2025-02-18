91aa08a10c8c8f34fa25df19cd75417397146859
Annu Priya <153050969+priyasinghanu@users.noreply.github.com>
2025-02-18 16:52:17 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+import Layout from './Layout'
+function App() {
+ 
+  return (
+    
+<Layout> 
+<h1> Resturent website</h1>
+</Layout>
+
+  )
+}
+
+export default App
---
+++ Layout.jsx
@@ -0,0 +1,17 @@
+//import React from 'React'
+
+import Header from './header'
+
+const Layout = () => {
+  return (
+<>
+    <Header>
+      <h1>annu</h1>
+    </Header>
+  
+</>
+    
+  )
+}
+
+export default Layout
